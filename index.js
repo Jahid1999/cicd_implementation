b9668e525062802f56281b4d4ff98dc848501a7b
Jahid1999 <bsse1030@iit.du.ac.bd>
2021-12-07 19:21:15 +0600

    simple web app for cicd implementation

---
+++ index.js
@@ -0,0 +1,39 @@
+const { POINT_CONVERSION_COMPRESSED } = require('constants');
+const express = require('express');
+const app = express();
+
+const logger = (req, res, next) => {
+    // console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}}`);
+    next();
+};
+
+app.use(logger);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.get('/string/size/:str',async (req,res)=>{
+    var a = req.params.str;
+
+    res.send(`The size of string ${a} is ${a.length}.`);
+})
+
+app.get('/string/concat/:str1/:str2',async (req,res)=>{
+    var output = req.params.str1.concat(req.params.str2);
+    res.send(`String 1 = ${req.params.str1}, String 2 = ${req.params.str2},  
+    Output: ${output}`);   
+})
+
+app.get('/number/add/:n1/:n2',async (req,res)=>{
+    var n1 = parseInt(req.params.n1);
+    var n2 = parseInt(req.params.n2);
+    var sum = n1+n2;
+
+    res.send(`Sum of ${n1} and ${n2} = ${sum}.`);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server Running on port: ${PORT}`);
+});
